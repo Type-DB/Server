d95f6dc733bf4ca22dac92230f94ef70101c12d2
Clint.Network <me@clint.network>
2019-01-20 01:53:53 +0100

    Create Web UI Base

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TypeDB.Server.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("authentication")]
+        public IActionResult Authentication()
+        {
+            return View();
+        }
+
+        [Route("triggers")]
+        public IActionResult Triggers()
+        {
+            return View();
+        }
+
+        [Route("migration")]
+        public IActionResult Migration()
+        {
+            return View();
+        }
+
+        [Route("scripts")]
+        public IActionResult Scripts()
+        {
+            return View();
+        }
+
+        [Route("settings")]
+        public IActionResult Settings()
+        {
+            return View();
+        }
+
+        [Route("account")]
+        public IActionResult Account()
+        {
+            return View();
+        }
+
+        [Route("lock")]
+        public IActionResult Lock()
+        {
+            return View();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -23,15 +16,7 @@ namespace TypeDB.Server
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -49,13 +34,12 @@ namespace TypeDB.Server
 
             //app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Default}/{action=Index}/{id?}");
             });
         }
     }
